9dd6554c125f99f6a1239e56e403973e2373879d
anazurcho <ana.zurabashvili.1@btu.edu.ge>
2021-08-18 03:22:48 +0400

    collection.count is deprecated, and will be removed in a future version. Use Collection.countDocuments or Collection.estimatedDocumentCount instead

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -60,7 +60,7 @@ router.get("", (req, res, next) => {
   postQuery
     .then((documents) => {
       fetchedPosts = documents;
-      return Post.count();
+      return Post.countDocuments();
     })
     .then((count) => {
       res.status(200).json({
