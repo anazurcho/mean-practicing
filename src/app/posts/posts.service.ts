379c05e3d8535668007614c9b6ba0f8cf4404519
anazurcho <ana.zurabashvili.1@btu.edu.ge>
2021-08-18 03:30:02 +0400

    fix error

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -114,6 +114,6 @@ export class PostsService {
   }
 
   deletePost(postId: string) {
-    return this.http.delete("http://localhost:3000/api/posts/" + postId);
+    return this.http.delete(environment.apiURL + "/posts/" + postId);
   }
 }
